37e990714c40d1ff73171f6a25b5c4806d885d93
Mohamed Asif <farookasif007@gmail.com>
2021-09-27 17:29:12 +0530

    removed new commit

--- Simple.java
+++ Simple.java
@@ -2,7 +2,6 @@ class Simple{
     public static void main(String args[]){  
      System.out.println("Hello Java");  
      System.out.println("Git and Jenkins integration demo"); 
-     System.out.println("Modified");
-System.out.println("job ");
+     System.out.println("Modified"); 
     }  
 }  
